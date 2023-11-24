dd0517ab097ad58c074b5bbf4fb13ea9f538e8d8
Srushti Madaiah Basavaraju <hvh9084@autuni.ac.nz>
2023-11-24 23:13:18 +1300

    files moved to repo

--- home.js
+++ home.js
@@ -1,1 +1,31 @@
 console.log("hello");
+//alert("yoo");
+
+//variables can store strings and integers 
+var a = 2;
+var b = 'smoothie';
+console.log(a);
+console.log(b);
+
+// you can take html code and by id and then display it through Javascript code
+
+  //var age = prompt("what is your age?");
+
+   //document.getElementById('someText').innerHTML = age;
+
+// Numbers in JavaScript 
+ // +  -  *  % 
+var num1  = 5.7;
+num1 = num1 + 1;
+console.log(num1);
+
+
+// Functions 
+ 
+
+
+// Here I have created a function I just have to call the function 
+function fun() {
+    alert('this is a function');
+}
+
